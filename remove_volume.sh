b19252a77b6de036affac7a47e9bc2dce04195a1
William Barshop <wbarshop@ucla.edu>
2017-05-10 16:55:25 -0700

    Accessory files to help replace volumes...

---
+++ remove_volume.sh
@@ -0,0 +1,1 @@
+sudo docker volume rm milkyway-stack_galaxy_storage
---
+++ replace_stack.sh
@@ -0,0 +1,5 @@
+sudo docker stack rm milkyway-stack && \
+sleep 45 && \
+sh remove_volume.sh && \
+sleep 5 && \
+sh deploy_stack.sh
